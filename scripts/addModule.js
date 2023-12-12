a7d6983c4749667851b0e81f50c64ef5d50b31af
Odin Contreras <odin@foreach.cl>
2023-12-11 20:43:41 -0400

    add helpers scripts

--- scripts/addModule.js
+++ scripts/addModule.js
@@ -1,29 +1,34 @@
-const fs = require('fs')
-const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
 // Specify the path of the new folder
 
 (() => {
-  const [moduleName] = process.argv.slice(2)
+  const [moduleName] = process.argv.slice(2);
 
   if (!moduleName) {
-    console.error('No module name provided')
-    process.exit(1)
+    console.error("No module name provided");
+    process.exit(1);
   }
 
-  const newModulePath = path.join(process.cwd(), 'src', 'modules', moduleName)
+  const newModulePath = path.join(process.cwd(), "src", "modules", moduleName);
 
-  fs.mkdirSync(newModulePath)
+  fs.mkdirSync(newModulePath);
 
-  const controllersPath = path.join(newModulePath, `${moduleName}.controllers.ts`)
-  const servicesPath = path.join(newModulePath, `${moduleName}.services.ts`)
-  const routesPath = path.join(newModulePath, `${moduleName}.routes.ts`)
+  const controllersPath = path.join(
+    newModulePath,
+    `${moduleName}.controllers.ts`,
+  );
+  const servicesPath = path.join(newModulePath, `${moduleName}.services.ts`);
+  const routesPath = path.join(newModulePath, `${moduleName}.routes.ts`);
 
-  const paths = [controllersPath, servicesPath, routesPath]
+  const paths = [controllersPath, servicesPath, routesPath];
 
   paths.forEach((path) => {
-    fs.writeFileSync(path, '')
-  })
+    fs.writeFileSync(path, "");
+  });
 
-  console.log('Module added successfully.')
-})()
+  console.log("Module added successfully.");
+})();
