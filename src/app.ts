c5e68d5b2bd86fc00faf33cb758405a3aea55589
Odin Contreras <odin@foreach.cl>
2023-12-21 11:50:57 -0400

    add express-query-parser dependency

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import "dotenv/config";
-import express from "express";
 import cors from "cors";
+import { queryParser } from "express-query-parser";
+import express from "express";
 import routes from "./routes";
 import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(cors());
+app.use(
+  queryParser({
+    parseNull: true,
+    parseUndefined: true,
+    parseBoolean: true,
+    parseNumber: true,
+  }),
+);
 app.use(express.json());
 app.use(routes);
 app.get("/ping", (_, res) => {
